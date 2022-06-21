60f633ffac70762a210c43dbe00a799a0feaac04
Kristine Kochie <kristinekochie@gmail.com>
2022-06-21 15:40:17 -0400

    Basic CSS styles

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+    //Fetch to The Color API
+    // fetch('https://www.thecolorapi.com/id?hex=0047AB')
+    // .then(res => res.json())
+    // .then(console.log)
 
-    console.log('hello')
+    //Fetch to Colormind API
+    // fetch('http://colormind.io/list/')
+    // .then(res => res.json())
+    // .then(console.log)
 
 })
